54c5a3414dd4625a1dead95e7f742aeb5adaf661
KondiGitHub <haichary@yahoo.co.in>
2018-05-18 23:31:08 -0400

    Adding another jsp

---
+++ src/com/simpleprogrammer/actions/EnterProtien.java
@@ -0,0 +1,13 @@
+package com.simpleprogrammer.actions;
+
+import com.opensymphony.xwork2.Action;
+
+public class EnterProtien implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+}
