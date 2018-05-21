36cc903063e907e32e6d2367f4908eb9f33e6734
KondiGitHub <haichary@yahoo.co.in>
2018-05-21 18:48:18 -0400

    Adding redirect on action

--- src/com/simpleprogrammer/actions/HelloAction.java
+++ src/com/simpleprogrammer/actions/HelloAction.java
@@ -1,27 +1,23 @@
 package com.simpleprogrammer.actions;
 
 import com.opensymphony.xwork2.Action;
+import org.apache.struts2.convention.annotation.Result;
+
 
 public class HelloAction implements Action {
 	
 	private String greeting;
 
 	@Override
-	@org.apache.struts2.convention.annotation.Action("hi")
+	@org.apache.struts2.convention.annotation.Action(value="hi",
+	results={@Result(name="success",location="enter-protein",type="redirect")})
 	public String execute() throws Exception {
 		
 		setGreeting("Hello Struts 2!!!!!!");
 		
-		return "do-more";
+		return "success";
 	}
 	
-	@org.apache.struts2.convention.annotation.Action("hi-do-more")
-	public String doMore() throws Exception {
-		
-		setGreeting("Hello Struts ####!!!!!!");
-		
-		return "hello";
-	}
 	
 	public String getGreeting() {
 		return greeting;
