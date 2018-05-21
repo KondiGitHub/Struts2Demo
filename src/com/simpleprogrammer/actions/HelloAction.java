8d840fe3b493cfdac4da06c221620c9ab1e610fb
KondiGitHub <haichary@yahoo.co.in>
2018-05-21 18:21:29 -0400

    Adding chaining

--- src/com/simpleprogrammer/actions/HelloAction.java
+++ src/com/simpleprogrammer/actions/HelloAction.java
@@ -7,11 +7,20 @@ public class HelloAction implements Action {
 	private String greeting;
 
 	@Override
+	@org.apache.struts2.convention.annotation.Action("hi")
 	public String execute() throws Exception {
 		
 		setGreeting("Hello Struts 2!!!!!!");
 		
-		return "success";
+		return "do-more";
+	}
+	
+	@org.apache.struts2.convention.annotation.Action("hi-do-more")
+	public String doMore() throws Exception {
+		
+		setGreeting("Hello Struts ####!!!!!!");
+		
+		return "hello";
 	}
 	
 	public String getGreeting() {
